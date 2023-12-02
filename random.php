d84a9fcff0c720e35f150fd63e4e223fd82e75d6
Jorge Esquén <shingekyichijo@gmail.com>
2023-12-01 21:51:29 -0500

    Prueba #2 usar y no descargar

--- random.php
+++ random.php
@@ -2,13 +2,15 @@
     header("Content-Type: text/html"); // Ajusta el tipo de contenido según sea necesario
     $rango1 = 0;
     $rango2 = 34;
-    $num = rand($rango1,$rango2);
-    if ($num == $rango1){
+    $num = rand($rango1, $rango2);
+
+    if ($num == $rango1) {
         $resultado = "No hay ni rastro de haber existido tula";
-    }if ($num == $rango2){
-        $resultado = "Es tan grande que no le cabe en el pantalon";
-    }else{
-        $resultado = "Le mide: ".$num;
+    } elseif ($num == $rango2) {
+        $resultado = "Es tan grande que no le cabe en el pantalón";
+    } else {
+        $resultado = "Le mide: " . $num;
     }
-    echo $resultado
+
+    echo $resultado;
 ?>
