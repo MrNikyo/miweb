e1761b1328060ea78a9bc602b4a96c57281e999c
Jorge Esquén <shingekyichijo@gmail.com>
2023-12-01 21:36:53 -0500

    juas

--- random.php
+++ random.php
@@ -2,11 +2,12 @@
     $rango1 = 0;
     $rango2 = 34;
     $num = rand($rango1,$rango2);
-    if ($num == $rango1) 
+    if ($num == $rango1){
         $resultado = "No hay ni rastro de haber existido tula";
-    if ($num == $rango2)
+    }if ($num == $rango2){
         $resultado = "Es tan grande que no le cabe en el pantalon";
-    else
-        $resultado = "Le mide: " + $num;
-    return $resultado
+    }else{
+        $resultado = "Le mide: ".$num;
+    }
+    echo $resultado
 ?>
